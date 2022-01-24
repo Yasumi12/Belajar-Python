c6126b3285b83da6491b584df334adec70bcb048
Yasumi12 <75873816+Yasumi12@users.noreply.github.com>
2022-01-24 07:27:56 +0700

    Create Main.py

---
+++ 02 - Variable/Main.py
@@ -0,0 +1,20 @@
+# Variabel adalah tempat untuk menyimpan data
+
+a = 10
+x = 5
+panjang = 100
+
+# lakukan pemanggilan
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+# Penamaan
+# Penamaan varaiable tidak boleh menggunakan spasi dan juga simbol selain underscore
+
+nama = "Akbar"
+usia = 18
+
+# Pemanggilan
+print("Nama = ", nama)
+print("Usia = ", usia)
